25eecae4cee7b56a13eb5bdf49950f18e0e8d0f2
rosera <askrichardrose@gmail.com>
2021-07-07 13:38:30 +0000

    Add list example

---
+++ dart/lab01/solutions/hello-list.dart
@@ -0,0 +1,8 @@
+
+
+void main() {
+  List<String> daysInWeek = ['mon', 'tues', 'wed', 'thurs', 'fri'];
+  print('Hello $daysInWeek');
+  print('Hello ${daysInWeek[0]}');
+  print('Hello ${daysInWeek.length}');
+}
